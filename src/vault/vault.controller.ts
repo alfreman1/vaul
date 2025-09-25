7e4cc61c04215ab5d4df3a61cfb23d3e6336d22b
alfreman1 <carlos.manjon@ucb.edu.bo>
2025-09-24 23:29:47 -0400

    featÃ: vault controller

--- src/vault/vault.controller.ts
+++ src/vault/vault.controller.ts
@@ -7,45 +7,50 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { VaultService } from './vault.service';
 import { CreateVaultDto } from './dto/create-vault.dto';
 import { UpdateVaultDto } from './dto/update-vault.dto';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { VaultEntity } from './entities/vault.entity';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('vault')
 @ApiTags('vault')
+@UseGuards(JwtAuthGuard)
 export class VaultController {
   constructor(private readonly vaultService: VaultService) {}
 
   @Post()
+  @ApiBearerAuth()
   @ApiCreatedResponse({ type: VaultEntity })
   async create(@Body() createVaultDto: CreateVaultDto) {
     return new VaultEntity(await this.vaultService.create(createVaultDto));
   }
 
   @Get()
+  @ApiBearerAuth()
   @ApiOkResponse({ type: VaultEntity, isArray: true })
   async findAll() {
     const vault = await this.vaultService.findAll();
     return vault.map((vault) => new VaultEntity(vault));
   }
 
-  @Get('drafts')
-  @ApiOkResponse({ type: VaultEntity, isArray: true })
-  async findDrafts() {
-    const drafts = await this.vaultService.findDrafts();
-    return drafts.map((draft) => new VaultEntity(draft));
-  }
-
   @Get(':id')
+  @ApiBearerAuth()
   @ApiOkResponse({ type: VaultEntity })
   async findOne(@Param('id', ParseIntPipe) id: number) {
     return new VaultEntity(await this.vaultService.findOne(id));
   }
 
   @Patch(':id')
+  @ApiBearerAuth()
   @ApiCreatedResponse({ type: VaultEntity })
   async update(
     @Param('id', ParseIntPipe) id: number,
@@ -55,6 +60,7 @@ export class VaultController {
   }
 
   @Delete(':id')
+  @ApiBearerAuth()
   @ApiOkResponse({ type: VaultEntity })
   async remove(@Param('id', ParseIntPipe) id: number) {
     return new VaultEntity(await this.vaultService.remove(id));
